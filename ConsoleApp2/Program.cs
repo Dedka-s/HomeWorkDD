33fe2c7c96748a4028c05b3fe32646717c1bbd32
Daniil <dedda2@yandex.ru>
2021-03-10 18:11:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using MyLibraryForDD;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string allText = File.ReadAllText("E:\\text.txt");
+
+            var workWithText = new WorkWithText();
+
+            var workWithTextType = typeof(WorkWithText);
+
+                        var methods = workWithTextType.GetMethods(BindingFlags.Instance
+                    | BindingFlags.Static
+                    | BindingFlags.Public
+                    | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+
+            object[] argsFor = new object[1];
+            argsFor[0] = allText;
+     
+                Dictionary<string, int> wordList = (Dictionary<string, int>)methods[0].Invoke(workWithText, argsFor);
+
+                using (FileStream fs = new FileStream("E:\\textResalt.txt", FileMode.OpenOrCreate))
+                {
+                    using (TextWriter tw = new StreamWriter(fs))
+
+                        foreach (KeyValuePair<string, int> kvp in wordList)
+                        {
+                            tw.WriteLine(string.Format("{0};{1}", kvp.Key, kvp.Value));
+                        }
+                }
+                                
+            }
+            
+    }
+}
---
+++ MyLibraryForDD/WorkWithText.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyLibraryForDD
+{
+    public class WorkWithText
+    {
+        private Dictionary<string, int> GetDictionary (String allText)
+        {
+            Regex reg_exp = new Regex("[^a-zA-Zа-яА-Я]");
+            allText = reg_exp.Replace(allText, " ");
+
+            string[] words = allText.Split(
+            new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+            var wordList = new Dictionary<string, int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToLower();
+                if (wordList.ContainsKey(words[i]))
+                {
+                    wordList[words[i]]++;
+                }
+                else
+                {
+                    wordList.Add(words[i], 1);
+                }
+            }
+
+            wordList = wordList.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            return wordList;
+        }
+    }
+}
