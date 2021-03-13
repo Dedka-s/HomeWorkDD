5c2cd165f43d1e6e2ffac1e1da4e75b3cceb444f
Daniil <dedda2@yandex.ru>
2021-03-13 13:07:21 +0300

    Home Work Thread

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -13,19 +15,32 @@ namespace ConsoleApp2
 
         static void Main(string[] args)
         {
+            long freq = Stopwatch.Frequency;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             string allText = File.ReadAllText("E:\\text.txt");
 
             var workWithText = new WorkWithText();
 
             var workWithTextType = typeof(WorkWithText);
 
-                        var methods = workWithTextType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+            var methods = workWithTextType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
 
             object[] argsFor = new object[1];
             argsFor[0] = allText;
+            Dictionary<string, int> wordList = null;
+            for (int k = 0; k < methods.Length; k++)
+            {
+                if (methods[k].Name.Equals("GetDictionary"))
+                {
+                    wordList = (Dictionary<string, int>)methods[k].Invoke(workWithText, argsFor);
+                }
+            }
      
-                Dictionary<string, int> wordList = (Dictionary<string, int>)methods[0].Invoke(workWithText, argsFor);
+           
+            
 
+            //Dictionary<string, int> wordList = workWithText.GetDictionaryOnThread(allText);
                 using (FileStream fs = new FileStream("E:\\textResalt.txt", FileMode.OpenOrCreate))
                 {
                     using (TextWriter tw = new StreamWriter(fs))
@@ -35,7 +50,10 @@ namespace ConsoleApp2
                             tw.WriteLine(string.Format("{0};{1}", kvp.Key, kvp.Value));
                         }
                 }
-                                
+            stopwatch.Stop();
+            double sec = (double)stopwatch.ElapsedTicks / freq;
+            Console.WriteLine(sec);
+            Console.ReadLine();              
             }
             
     }
--- MyLibraryForDD/WorkWithText.cs
+++ MyLibraryForDD/WorkWithText.cs
@@ -3,12 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MyLibraryForDD
 {
     public class WorkWithText
     {
+             static object locker = new object();
+             static Dictionary<string, int> wordListStream = new Dictionary<string, int>();
+        public static void GetOnThread(object paramObj)
+        {
+            Regex reg_exp = new Regex("[^a-zA-Zа-яА-Я]");
+            String p = (String)paramObj;
+            p = reg_exp.Replace(p, " ");
+            string[] words = p.Split(
+            new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+            
+                for (int i = 0; i < words.Length; i++)
+                {
+                    words[i] = words[i].ToLower();
+                lock (locker)
+                {
+                    if (wordListStream.ContainsKey(words[i]))
+                    {
+                        wordListStream[words[i]]++;
+                    }
+                    else
+                    {
+                       
+                        wordListStream.Add(words[i], 1);
+                    }
+                }
+            }
+        }
+
+        public Dictionary<string, int> GetDictionaryOnThread(String allText)
+        {
+            int i = allText.Length / 2;
+            while (allText[i] != ' ') {
+                i++;
+            }
+
+            Thread myThread1 = new Thread(new ParameterizedThreadStart(GetOnThread));
+            myThread1.Start(allText.Substring(0, allText.Length - i));
+            
+            
+            Thread myThread2 = new Thread(new ParameterizedThreadStart(GetOnThread));
+            myThread2.Start(allText.Substring(i));
+
+            myThread1.Join();
+            myThread2.Join();
+
+            wordListStream = wordListStream.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            return wordListStream;
+        }
+
         private Dictionary<string, int> GetDictionary (String allText)
         {
             Regex reg_exp = new Regex("[^a-zA-Zа-яА-Я]");
